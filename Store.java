705ad8606632d364d2e368b47da9407828bf25f2
Marcela Alonso <MARCELA.ALONSO.S@GMAIL.COM>
2023-03-13 19:05:01 -0400

    java methods in progress

--- Store.java
+++ Store.java
@@ -1,22 +1,22 @@
 public class Store {
   //instance fields
   String productType;
-  int inventoryCount;
-  double inventoryPrice;
 
   //constructor method
-  public Store(String product, int count, double price) {
+  public Store(String product) {
     productType = product;
-    inventoryCount = count;
-    inventoryPrice = price;
+  }
+
+  //advertise method
+  public void advertise() {
+    System.out.println("Come spend some money!");
+    System.out.println("Selling " + productType + "!");
   }
 
   //main method
-  public static void main(String[] args) {
-    Store lemonadeStand = new Store("lemonade", 42, .99);
-    Store cookieShop = new Store("cookies", 12, 3.75);
+  public static void main(String[] args){
+    Store lemonadeStand = new Store("Lemonade");
+    lemonadeStand.advertise();
 
-    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
-    System.out.println("Our second shop has " +cookieShop.inventoryCount + " units remaining.");
   }
 }
