1c930b87e10937d35a2096cc7461667df050d6c6
Marcela Alonso <MARCELA.ALONSO.S@GMAIL.COM>
2023-03-20 11:05:44 -0400

    loops 95% done

--- Loops/Lunch.java
+++ Loops/Lunch.java
@@ -2,8 +2,13 @@ import java.util.ArrayList;
 
 class Lunch {
    public static ArrayList<String> removeAnts(ArrayList<String> lunchBox) {
-
-
+      for (int i = 0; i < lunchBox.size(); i++) {
+         if (lunchBox.get(i) == "ant") {
+            lunchBox.remove(lunchBox.get(i));
+            i--;
+         }
+      }
+      return lunchBox;
    }
 
    public static void main(String[] args) {
---
+++ PrimeDetective.java
@@ -0,0 +1,9 @@
+class PrimeDetective {
+
+
+  public static void main(String[] args) {
+
+    PrimeDetective pd = new PrimeDetective();
+    int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+  }
+}
